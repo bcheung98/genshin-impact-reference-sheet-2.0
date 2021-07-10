c762b727fa16c0d04f40abf8c948feba002f3021
Brian <briantcheung@gmail.com>
2021-07-09 21:01:08 -0700

    Set up basic home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+    headerBar: {
+        textAlign: "center",
+        padding: 10
+    },
+    footerBar: {
+        top: "auto",
+        bottom: 0,
+        padding: 10,
+        textAlign: "center"
+    }
+}))
+
+const App = () => {
+    const classes = useStyles();
     return (
-        <div className="App">
-            <header className="App-header">
-    
-            </header>
-        </div>
+        <Router>
+            <AppBar position="static" className={classes.headerBar}>
+                    <Typography variant="h4">Genshin Impact Reference Sheet</Typography> 
+            </AppBar>
+            <AppBar position="fixed" className={classes.footerBar}>
+                    <Typography variant="subtitle2">Genshin Impact™ is a registered trademark of MiHoYo Co., Ltd.</Typography>
+                    <Typography variant="subtitle2">Images and data ©MiHoYo Co., Ltd.</Typography>
+                    <Typography variant="subtitle2">This website was made for my own education and research into making React applications.</Typography>
+            </AppBar>
+        </Router>
     );
 }
 
