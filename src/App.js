a3b7a0cef872d9a9b952d2501d3ddb119ffff1b5
Brian <briantcheung@gmail.com>
2021-07-10 23:43:56 -0700

    Removed router stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import './App.css';
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Redirect,
-    Switch
-} from "react-router-dom";
-
 import CharacterBrowser from './components/CharacterBrowser';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -30,31 +23,18 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
     const classes = useStyles();
-
     return (
-        <Router>
-
+        <React.Fragment>
             <AppBar position="static" className={classes.headerBar}>
                 <Typography variant="h3">Genshin Impact Reference Sheet</Typography>
             </AppBar>
-
-            <Switch>
-
-                <Route exact path="/" component={() => (
-                    <Redirect to="/characters" />
-                )} />
-
-                <Route path="/characters" component={CharacterBrowser} />
-
-            </Switch>
-
+            <CharacterBrowser />
             <AppBar position="static" className={classes.footerBar}>
                 <Typography variant="subtitle2">Genshin Impact™ is a registered trademark of MiHoYo Co., Ltd.</Typography>
                 <Typography variant="subtitle2">Images and data ©MiHoYo Co., Ltd.</Typography>
                 <Typography variant="subtitle2">This website was made for my own education and research into making React applications.</Typography>
             </AppBar>
-
-        </Router>
+        </React.Fragment> 
     );
 }
 
--- src/components/CharacterBrowser.js
+++ src/components/CharacterBrowser.js
@@ -66,4 +66,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(CharacterBrowser));
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterBrowser);
