0efb3389e823a9a20eed3c8ff7b639340e5605af
Brian <briantcheung@gmail.com>
2021-07-10 23:30:43 -0700

    Filter css

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,6 +5,7 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Divider from '@material-ui/core/Divider';
 
 import "../filters.css";
 import ElementFilter from "./filters/ElementFilter";
@@ -18,10 +19,6 @@ const useStyles = makeStyles((theme) => ({
         width: "70%",
         marginBottom: "15px"
     },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
     bar: {
         color: "white",
         backgroundColor: "rgb(32, 32, 32)",
@@ -30,6 +27,9 @@ const useStyles = makeStyles((theme) => ({
     filterRow: {
         margin: "5px",
         padding: 0
+    },
+    divider: {
+        backgroundColor: "gray"
     }
 }));
 
@@ -39,17 +39,21 @@ const Filters = () => {
         <div className={classes.root}>
             <Accordion className={classes.bar}>
                 <AccordionSummary expandIcon={<ExpandMoreIcon color="secondary" />}>
-                    <Typography className={classes.heading}>Filters</Typography>
+                    <Typography variant="h6">Filters</Typography>
                 </AccordionSummary>
+                <Divider className={classes.divider} />
                 <AccordionDetails className={classes.filterRow}>
                     <ElementFilter />
                 </AccordionDetails>
+                <Divider className={classes.divider} />
                 <AccordionDetails className={classes.filterRow}>
                     <WeaponFilter />
                 </AccordionDetails>
+                <Divider className={classes.divider} />
                 <AccordionDetails className={classes.filterRow}>
                     <TalentFilter />
                 </AccordionDetails>
+                <Divider className={classes.divider} />
                 <AccordionDetails className={classes.filterRow}>
                     <BossMatFilter />
                 </AccordionDetails>
