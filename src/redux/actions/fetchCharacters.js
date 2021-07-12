d655eaa4c9976de06d0e826151f4a7225ee00a29
Brian <briantcheung@gmail.com>
2021-07-11 17:52:16 -0700

    Switched character db to new source

--- src/redux/actions/fetchCharacters.js
+++ src/redux/actions/fetchCharacters.js
@@ -1,7 +1,7 @@
 export const fetchCharacters = () => {
     return (dispatch) => {
         dispatch({ type: "START_GETTING_CHARS_REQUEST" });
-        fetch("https://bcheung98.github.io/genshin-impact-character-db/characters.json")
+        fetch("https://bcheung98.github.io/genshin-impact-character-db-2.0/characters.json")
             .then(res => res.json())
             .then(characters => dispatch({ type: "GET_CHARS", characters }));
     }
