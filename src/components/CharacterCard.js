6fc997c6417d7fc4738c296bff66906ac823108d
Brian <briantcheung@gmail.com>
2021-07-13 13:30:02 -0700

    Added highlighted text

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -19,6 +19,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
+import "../textColor.css";
 
 const useStyles = makeStyles((theme) => ({
     root: {
