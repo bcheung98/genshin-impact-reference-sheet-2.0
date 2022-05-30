eb1ce877ffe80356e39e6666c56fe0cd4eda8e70
Brian <briantcheung@gmail.com>
2022-05-30 00:09:06 -0700

    Removed test db link

--- src/redux/actions/fetchCharacters.js
+++ src/redux/actions/fetchCharacters.js
@@ -1,8 +1,7 @@
 export const fetchCharacters = () => {
     return (dispatch) => {
         dispatch({ type: "START_GETTING_CHARS_REQUEST" });
-        // https://bcheung98.github.io/genshin-impact-character-db-2.0/characters.json
-        fetch("http://localhost:3000/characters")
+        fetch("https://bcheung98.github.io/genshin-impact-character-db-2.0/characters.json")
             .then(res => res.json())
             .then(characters => dispatch({ type: "GET_CHARS", characters }));
     }
