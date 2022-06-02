b799d298dd2cc8a745320ac0e43df6cc97f7e2f7
Brian <briantcheung@gmail.com>
2022-06-01 23:04:56 -0700

    Added search function

--- src/components/CharacterBrowser.js
+++ src/components/CharacterBrowser.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchCharacters } from "../redux/actions/fetchCharacters";
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import ViewComfyIcon from '@material-ui/icons/ViewComfy';
-import ListIcon from '@material-ui/icons/List';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import ViewComfyIcon from "@material-ui/icons/ViewComfy";
+import ListIcon from "@material-ui/icons/List";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
 
 import Filters from "./Filters";
 import CharacterCard from "./CharacterCard";
@@ -28,9 +30,26 @@ const useStyles = makeStyles((theme) => ({
     toggleIcon: {
         color: "white",
     },
+    inputRoot: {
+        border: "2px solid gray",
+        borderRadius: "5px",
+        backgroundColor: "rgb(44, 49, 64)",
+        display: "flex",
+        margin: "auto",
+        height: "40px",
+        width: "84%",
+        marginTop: "10px",
+        marginLeft: "20px",
+    },
+    inputTextField: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+        color: "white",
+        fontFamily: "Genshin, sans-serif",
+    },
 }));
 
-const filterCharacters = (characters, filters) => {
+const filterCharacters = (characters, filters, searchValue) => {
     let chars = [...characters];
     if (filters.element.length > 0) {
         chars = chars.filter(char => filters.element.includes(char.element));
@@ -53,6 +72,9 @@ const filterCharacters = (characters, filters) => {
     if (filters.nation.length > 0) {
         chars = chars.filter(char => filters.nation.includes(char.nation));
     }
+    if (searchValue !== "") {
+        chars = chars.filter(char => char.name.toLowerCase().includes(searchValue.toLowerCase()))
+    }
 
     return chars
 }
@@ -68,6 +90,12 @@ const CharacterBrowser = (props) => {
     const toggleChecked = () => {
         setChecked((prev) => !prev);
     };
+
+    const [searchValue, setSearchValue] = React.useState("");
+
+    const handleInputChange = (e) => {
+        setSearchValue(e.target.value);
+    }
 
     let { characters, fetchCharacters, filters } = props;
 
@@ -89,15 +117,22 @@ const CharacterBrowser = (props) => {
             </div>
             <Grid container>
                 <Grid item xs={3}>
+                    <Paper className={classes.inputRoot}>
+                        <InputBase
+                            className={classes.inputTextField}
+                            placeholder="Search"
+                            onChange={handleInputChange}
+                        />
+                    </Paper>
                     <Filters />
                 </Grid>
                 <Grid item xs={9}>
                     {!checked ?
                         (<Grid container className={classes.root}>
-                            {characters.characters.length > 0 ? filterCharacters(characters.characters, filters).map(char => <CharacterCard key={char.id} character={char} />) : null}
+                            {characters.characters.length > 0 ? filterCharacters(characters.characters, filters, searchValue).map(char => <CharacterCard key={char.id} character={char} />) : null}
                         </Grid>)
                         :
-                        <CharacterList characters={filterCharacters(characters.characters, filters)} />
+                        <CharacterList characters={filterCharacters(characters.characters, filters, searchValue)} />
                     }
                 </Grid>
             </Grid>
