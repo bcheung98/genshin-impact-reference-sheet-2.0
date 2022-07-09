66db535c0306a97e79802630f0c737de99ef03aa
Brian <briantcheung@gmail.com>
2022-07-09 16:11:01 -0700

    Reordered character material grid

--- src/components/CharacterMaterialGrid.js
+++ src/components/CharacterMaterialGrid.js
@@ -25,22 +25,22 @@ const CharacterMaterialGrid = (props) => {
                 <MaterialTooltip title={formatTalents(talentBook)} arrow placement="top">
                     <img style={materialImage} src={require(`../assets/materials/talent_mats/${talentBook}.png`).default} alt={talentBook} />
                 </MaterialTooltip>
-                <MaterialTooltip title={formatCommonMats(commonMat)} arrow placement="top">
-                    <img style={materialImage} src={require(`../assets/materials/common_mats/${commonMat.split(" ").join("_")}.png`).default} alt={commonMat} />
-                </MaterialTooltip>
                 <MaterialTooltip title={formatWeeklyBossMats(weeklyBossMat)} arrow placement="top">
                     <img style={materialImage} src={require(`../assets/materials/weekly_boss_mats/${weeklyBossMat.split(" ").join("_")}.png`).default} alt={weeklyBossMat} />
                 </MaterialTooltip>
+                <MaterialTooltip title={formatCommonMats(commonMat)} arrow placement="top">
+                    <img style={materialImage} src={require(`../assets/materials/common_mats/${commonMat.split(" ").join("_")}.png`).default} alt={commonMat} />
+                </MaterialTooltip>
             </Grid>
             <Grid style={materialRow}>
-                <MaterialTooltip title={formatGemstone(element)} arrow placement="top">
-                    <img style={materialImage} src={require(`../assets/materials/ascension_gems/${element}_Gemstone.png`).default} alt={element} />
-                </MaterialTooltip>
                 <MaterialTooltip title={formatBossMats(bossMat)} arrow placement="top">
                     <img style={materialImage} src={require(`../assets/materials/boss_mats/${bossMat.split(" ").join("_")}.png`).default} alt={bossMat} />
                 </MaterialTooltip>
                 <MaterialTooltip title={localMat} arrow placement="top">
                     <img style={materialImage} src={require(`../assets/materials/local_specialties/${localMat.split(" ").join("_")}.png`).default} alt={localMat} />
+                </MaterialTooltip> 
+                <MaterialTooltip title={formatGemstone(element)} arrow placement="top">
+                    <img style={materialImage} src={require(`../assets/materials/ascension_gems/${element}_Gemstone.png`).default} alt={element} />
                 </MaterialTooltip>
             </Grid>
         </Grid>
